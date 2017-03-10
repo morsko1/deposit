0281ea7e827f63986328cfe13f20d6bde69bc212
morsko1 <morsko1@yandex.ru>
2017-03-10 22:51:46 +0300

    modified html and js

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-var sum = document.getElementById('sum'),
-	percent = document.getElementById('percent'),
-	term = document.getElementById('term'),
-	cap = document.getElementById('cap'),
-	result = document.getElementById('result'),
-	income = document.getElementById('income'),
-	calculate = document.getElementById('calculate');
-
-function calc () {
-	var sumVal = parseFloat(sum.value),
-		percentVal = parseFloat(percent.value),
-		termVal = parseFloat(term.value),
-		res = sumVal;
-	if (cap.checked) {
-		for (var i=0; i<termVal; i++) {
-			res = res * ((percentVal / 100) / 12 + 1);
+var app = (function () {
+	var sum = document.getElementById('sum'),
+		percent = document.getElementById('percent'),
+		term = document.getElementById('term'),
+		cap = document.getElementById('cap'),
+		result = document.getElementById('result'),
+		income = document.getElementById('income'),
+		calculate = document.getElementById('calculate'),
+		calc = function calc () {
+			var sumVal = parseFloat(sum.value),
+				percentVal = parseFloat(percent.value),
+				termVal = parseFloat(term.value),
+				res = sumVal;
+			if (cap.checked) {
+				for (var i=0; i<termVal; i++) {
+					res = res * ((percentVal / 100) / 12 + 1);
+				}
+				result.innerHTML = res.toFixed(2);
+				income.innerHTML = (res - sumVal).toFixed(2);
+			}
+			else {
+				res = res * (((percentVal / 100) / 12) * termVal + 1);
+				result.innerHTML = res.toFixed(2);
+				income.innerHTML = (res - sumVal).toFixed(2);
+			}
+		},
+		prevent = function () {
+			document.forms[0].addEventListener("submit", function (event) {
+				event.preventDefault();
+				return false;
+			});
 		}
-		result.innerHTML = res.toFixed(2);
-		income.innerHTML = (res - sumVal).toFixed(2);
-	}
-	else {
-		res = res * (((percentVal / 100) / 12) * termVal + 1);
-		result.innerHTML = res.toFixed(2);
-		income.innerHTML = (res - sumVal).toFixed(2);
-	}
-}
-calculate.addEventListener('click', calc);
+		init = function () {
+			prevent();
+			calculate.addEventListener('click', calc);
+		};
+		return {
+			init: init
+		};
+}());
+
+app.init();
